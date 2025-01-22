d13d27d69c1f69cfaef6aafd1fd454fc2cc69212
petr.shumnov <petr.shumnov@devexpress.com>
2025-01-22 17:11:31 +0200

    Fix CSS loading in published apps

--- CS/DevExpress.AI.Samples.WPFBlazor/Controls/ChatBlazorWebView.cs
+++ CS/DevExpress.AI.Samples.WPFBlazor/Controls/ChatBlazorWebView.cs
@@ -1,13 +1,18 @@
-﻿using DevExpress.AIIntegration.Blazor.Chat.WebView;
+﻿using System.IO;
+using DevExpress.AIIntegration.Blazor.Chat.WebView;
 using Microsoft.AspNetCore.Components.WebView.Wpf;
 using Microsoft.Extensions.FileProviders;
 
 namespace WPF_AIChatControl {
     internal class ChatBlazorWebView : BlazorWebView {
         public override IFileProvider CreateFileProvider(string contentRootDir) {
-            return new EmbeddedFileProvider(
-                typeof(IChatUIWrapper).Assembly,
-                typeof(ChatUIWrapper).Namespace);
+            var embeddedProvider = new EmbeddedFileProvider(
+                    typeof(ChatUIWrapper).Assembly,
+                    typeof(ChatUIWrapper).Namespace);
+            string wwwrootPath = Path.Combine(contentRootDir, "wwwroot");
+            if(Directory.Exists(wwwrootPath))
+                return new CompositeFileProvider([embeddedProvider, new PhysicalFileProvider(wwwrootPath)]);
+            return embeddedProvider;
         }
     }
 }
